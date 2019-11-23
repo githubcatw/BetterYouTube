c2c13d6c5f497d4477e2c2ca4b784753ff4b9752
Narek Torosyan <narek.email@gmail.com>
2019-11-23 20:59:57 +0400

    RIP YouTube Chat

--- inject.js
+++ inject.js
@@ -8,7 +8,5 @@ _BYTF_style.href = "https://fonts.googleapis.com/css?family=Roboto:100,300,300it
 // remove the button text
 document.getElementsByClassName("style-scope ytd-button-renderer style-default size-default")[3].innerHTML = "";
 document.getElementsByClassName("style-scope ytd-button-renderer style-default size-default")[5].innerHTML = "";
-// erase chat
-document.getElementsByClassName("style-scope ytd-notification-topbar-button-renderer")[0].innerHTML = "";
 
 })();
